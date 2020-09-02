fe28414d2c91e8d385a097275613d5ee58828de5
Hawyar <hawyarxoshnaw@gmail.com>
2020-09-02 00:58:45 -0400

    omit first / from paths

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ var fs = require('fs');
  */
 function mkdirSync(directory) {
   const paths = directory.replace(/\/$/, '').split('/');
+  if (paths[0] == '') {
+    paths.shift();
+  }
 
   paths.forEach((_, index, dir) => {
     index += 1;
+
     const path = dir.slice(0, index).join('/');
- 
     !fs.existsSync(path) ? fs.mkdirSync(path) : null;
   });
 }
+
+mkdirSync('/crazy/brazy');
 
 module.exports = {
   mkdirSync,

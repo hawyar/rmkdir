1036c34bfbf2fe6062c707364e447a991d6380e8
Hawyar <hawyarxoshnaw@gmail.com>
2020-09-01 22:09:49 -0400

    lets get it 🍞

---
+++ index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+/**
+ * @param {String} directory
+ */
+function mkdirSync(directory) {
+  const paths = directory.replace(/\/$/, '').split('/');
+
+  paths.forEach((_, index, dir) => {
+    index += 1;
+    const path = dir.slice(0, index).join('/');
+
+    !fs.existsSync(path) ? fs.mkdirSync(path) : null;
+  });
+}
+
+module.exports = {
+  mkdirSync,
+};
